43f2ca9de0ba31ba4bac68383115af3f5f814d34
Chris Morley <chris.morley@igentics.com>
2020-05-26 14:54:00 +0100

    Removed code which was ancestored in from inital copy of ContentPicker controller

--- src/ComplX.MailchimpPicker.Web/App_Plugins/MailchimpPicker/ComplX.MailchimpPicker.Controllers.js
+++ src/ComplX.MailchimpPicker.Web/App_Plugins/MailchimpPicker/ComplX.MailchimpPicker.Controllers.js
@@ -49,8 +49,6 @@ function mailchimpListPickerController($scope, entityResource, editorState, icon
     //the default pre-values
     var defaultConfig = {
         multiPicker: false,
-        showOpenButton: false,
-        showEditButton: false,
         showPathOnHover: false,
         dataTypeId: null,
         maxNumber: 1,
@@ -78,18 +76,11 @@ function mailchimpListPickerController($scope, entityResource, editorState, icon
         //merge the server config on top of the default config, then set the server config to use the result
         $scope.model.config = angular.extend(defaultConfig, $scope.model.config);
     }
-    //Umbraco persists boolean for prevalues as "0" or "1" so we need to convert that!
-    $scope.model.config.multiPicker = $scope.model.config.multiPicker === '1' ? true : false;
-    $scope.model.config.showOpenButton = $scope.model.config.showOpenButton === '1' ? true : false;
-    $scope.model.config.showEditButton = $scope.model.config.showEditButton === '1' ? true : false;
-    $scope.model.config.showPathOnHover = $scope.model.config.showPathOnHover === '1' ? true : false;
-    var entityType = $scope.model.config.startNode.type === 'member' ? 'Member' : $scope.model.config.startNode.type === 'media' ? 'Media' : 'Document';
-    $scope.allowOpenButton = entityType === 'Document';
-    $scope.allowEditButton = entityType === 'Document';
+
+    var entityType = 'Document';
     $scope.allowRemoveButton = true;
     //the dialog options for the picker
     var dialogOptions = {
-        multiPicker: $scope.model.config.multiPicker,
         entityType: entityType,
         filterCssClass: 'not-allowed not-published',
         startNodeId: null,
@@ -112,43 +103,6 @@ function mailchimpListPickerController($scope, entityResource, editorState, icon
     //since most of the pre-value config's are used in the dialog options (i.e. maxNumber, minNumber, etc...) we'll merge the
     // pre-value config on to the dialog options
     angular.extend(dialogOptions, $scope.model.config);
-    //We need to manually handle the filter for members here since the tree displayed is different and only contains
-    // searchable list views
-    if (entityType === 'Member') {
-        //first change the not allowed filter css class
-        dialogOptions.filterCssClass = 'not-allowed';
-        var currFilter = dialogOptions.filter;
-        //now change the filter to be a method
-        dialogOptions.filter = function (i) {
-            //filter out the list view nodes
-            if (i.metaData.isContainer) {
-                return true;
-            }
-            if (!currFilter) {
-                return false;
-            }
-            //now we need to filter based on what is stored in the pre-vals, this logic duplicates what is in the treepicker.controller,
-            // but not much we can do about that since members require special filtering.
-            var filterItem = currFilter.toLowerCase().split(',');
-            var found = filterItem.indexOf(i.metaData.contentType.toLowerCase()) >= 0;
-            if (!currFilter.startsWith('!') && !found || currFilter.startsWith('!') && found) {
-                return true;
-            }
-            return false;
-        };
-    }
-    if ($routeParams.section === 'settings' && $routeParams.tree === 'documentTypes') {
-        //if the content-picker is being rendered inside the document-type editor, we don't need to process the startnode query
-        dialogOptions.startNodeId = -1;
-    } else if ($scope.model.config.startNode.query) {
-        //if we have a query for the startnode, we will use that.
-        var rootId = $routeParams.id;
-        entityResource.getByQuery($scope.model.config.startNode.query, rootId, 'Document').then(function (ent) {
-            dialogOptions.startNodeId = $scope.model.config.idType === 'udi' ? ent.udi : ent.id;
-        });
-    } else {
-        dialogOptions.startNodeId = $scope.model.config.startNode.id;
-    }
     //dialog
     $scope.openContentPicker = function () {
         $scope.contentPickerOverlay = dialogOptions;
@@ -174,45 +128,17 @@ function mailchimpListPickerController($scope, entityResource, editorState, icon
         $scope.renderModel.splice(index, 1);
         angularHelper.getCurrentForm($scope).$setDirty();
     };
-    $scope.showNode = function (index) {
-        var item = $scope.renderModel[index];
-        var id = item.id;
-        var section = $scope.model.config.startNode.type.toLowerCase();
-        entityResource.getPath(id, entityType).then(function (path) {
-            navigationService.changeSection(section);
-            navigationService.showTree(section, {
-                tree: section,
-                path: path,
-                forceReload: false,
-                activate: true
-            });
-            var routePath = section + '/' + section + '/edit/' + id.toString();
-            $location.path(routePath).search('');
-        });
-    };
     $scope.add = function (item) {
         var currIds = _.map($scope.renderModel, function (i) {
             return $scope.model.config.idType === 'udi' ? i.udi : i.id;
         });
         var itemId = $scope.model.config.idType === 'udi' ? item.udi : item.id;
         if (currIds.indexOf(itemId) < 0) {
-            setEntityUrl(item);
+            addSelectedItem(item);
         }
     };
     $scope.clear = function () {
         $scope.renderModel = [];
-    };
-    $scope.openMiniEditor = function (node) {
-        miniEditorHelper.launchMiniEditor(node).then(function (updatedNode) {
-            // update the node
-            node.name = updatedNode.name;
-            node.published = updatedNode.hasPublishedVersion;
-            if (entityType !== 'Member') {
-                entityResource.getUrl(updatedNode.id, entityType).then(function (data) {
-                    node.url = data;
-                });
-            }
-        });
     };
     //when the scope is destroyed we need to unsubscribe
     $scope.$on('$destroy', function () {
@@ -223,55 +149,20 @@ function mailchimpListPickerController($scope, entityResource, editorState, icon
     //load current data if anything selected
     if ($scope.model.value !== "") {
         mailchimpPickerResource.getList($scope.model.value).then(function (result) {
-            setEntityUrl(result.data);
+            addSelectedItem(result.data);
 
             startWatch();
             subscribe();
-        });        
+        });
     } else {
         //everything is loaded, start the watch on the model
         startWatch();
         subscribe();
     }
-    function setEntityUrl(entity) {
-        // get url for content and media items
-        if (entityType !== 'Member') {
-            entityResource.getUrl(entity.id, entityType).then(function (data) {
-                // update url
-                angular.forEach($scope.renderModel, function (item) {
-                    if (item.id === entity.id) {
-                        if (entity.trashed) {
-                            item.url = localizationService.dictionary.general_recycleBin;
-                        } else {
-                            item.url = data;
-                        }
-                    }
-                });
-            });
-        }
-        // add the selected item to the renderModel
-        // if it needs to show a url the item will get
-        // updated when the url comes back from server
-        addSelectedItem(entity);
-    }
     function addSelectedItem(item) {
         // set icon
         if (item.icon) {
             item.icon = iconHelper.convertFromLegacyIcon(item.icon);
-        }
-        // set default icon
-        if (!item.icon) {
-            switch (entityType) {
-                case 'Document':
-                    item.icon = 'icon-document';
-                    break;
-                case 'Media':
-                    item.icon = 'icon-picture';
-                    break;
-                case 'Member':
-                    item.icon = 'icon-user';
-                    break;
-            }
         }
         $scope.renderModel.push({
             'name': item.name,
